a3903dc4d1d31708e04bc1f8374f6b028d79fbc9
Abdulrahman <bhabdulrahaman@gmail.com>
2022-07-19 11:21:06 +0100

    progress

---
+++ script.js
@@ -0,0 +1,60 @@
+"use strict";
+console.log("working");
+let display = document.querySelector(".display").value;
+
+const clearBtn = document.querySelector(".clr_btn");
+clearBtn.addEventListener("click", () => {
+  display = "";
+});
+
+//Add function
+function add(a, b) {
+  const sum = a + b;
+  return sum;
+}
+
+//difference funtion
+function difference(a, b) {
+  const diff = a - b;
+  return diff;
+}
+//multiply function
+function multiply(a, b) {
+  const product = a * b;
+  return product;
+}
+//divide function
+function divide(a, b) {
+  const divider = a / b;
+  return divider;
+}
+// console.log(divide(2, 3));
+function operate(a, b, operator) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return difference(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  } else operator === "*";
+  return multiply(a, b);
+}
+console.log(operate(2, 3, "-"));
+
+const displayValue = function pickValue() {
+  let keys = document.querySelectorAll(".btn");
+
+  keys.forEach((key) => {
+    key.addEventListener("click", () => {
+      const clickedKey = key.textContent;
+      console.log(clickedKey);
+      display += clickedKey;
+      console.log(display);
+      const error = (document.querySelector(".display").value = display);
+      console.log(error);
+    });
+  });
+};
+// pickValue();
+
+displayValue();
